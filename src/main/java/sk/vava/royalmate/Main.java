faa2dc3984417799027b8d6f00dae49dbcb20ab0
adamg <xgabora@stuba.sk>
2025-04-20 17:40:00 +0200

    Added correct MAVEN configuration for .jar building.

---
+++ src/main/java/sk/vava/royalmate/Main.java
@@ -0,0 +1,7 @@
+package sk.vava.royalmate;
+
+public class Main {
+    public static void main(String[] args) {
+        RoyalMate.main(args);
+    }
+}
--- src/main/java/sk/vava/royalmate/RoyalMate.java
+++ src/main/java/sk/vava/royalmate/RoyalMate.java
@@ -7,10 +7,10 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class RoyalMate extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(RoyalMate.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1080, 768);
         stage.setTitle("Hello WORLDDDD!");
         stage.setScene(scene);
